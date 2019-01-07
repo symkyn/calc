56937a0a365aa59a5d9a493662da561e89b1692e
symkyn <everett.blake@gamil>
2019-01-07 13:43:21 -0700

    some functionality

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -11,6 +11,42 @@ class Calculator extends Component {
             answer: 0,
             history: [],
         }
+
+        this.addNumber = this.addNumber.bind(this);
+        this.addDecimal = this.addDecimal.bind(this);
+        this.clear = this.clear.bind(this);
+        this.clearAll = this.clearAll.bind(this);
+    }
+
+    addNumber(num) {
+        if(this.state.answer === 0) {
+            this.setState({
+                answer: num
+            })
+        } else {
+            var newNumber = Number(this.state.answer.toString() + num.toString());
+            this.setState({
+                answer: newNumber
+            })
+        }
+    }
+
+    addDecimal() {
+        this.setState({
+            answer: this.state.answer.toString() + '.'
+        })
+    }
+
+    clear() {
+        this.setState({
+            answer: 0
+        })
+    }
+
+    clearAll() {
+        this.setState({
+            history: []
+        })
     }
 
     render() {
@@ -22,22 +58,26 @@ class Calculator extends Component {
                 <div className='manipulate'>
                     <div classname='first'>
                         <div className='changers'>
-                            <CalcButton>AC</CalcButton>
+                            {
+                                this.state.answer === 0 ?
+                                <CalcButton onClick={this.clearAll}>AC</CalcButton>
+                                : <CalcButton onClick={this.clear}>C</CalcButton>
+                            }
                             <CalcButton>-/+</CalcButton>
                             <CalcButton>%</CalcButton>
                         </div>
                         <div className='numbers'>
-                            <CalcButton>1</CalcButton>
-                            <CalcButton>2</CalcButton>
-                            <CalcButton>3</CalcButton>
-                            <CalcButton>4</CalcButton>
-                            <CalcButton>5</CalcButton>
-                            <CalcButton>6</CalcButton>
-                            <CalcButton>7</CalcButton>
-                            <CalcButton>8</CalcButton>
-                            <CalcButton>9</CalcButton>
-                            <CalcButton>0</CalcButton>
-                            <CalcButton>.</CalcButton>
+                            <CalcButton onClick={() => this.addNumber(1)}>1</CalcButton>
+                            <CalcButton onClick={() => this.addNumber(2)}>2</CalcButton>
+                            <CalcButton onClick={() => this.addNumber(3)}>3</CalcButton>
+                            <CalcButton onClick={() => this.addNumber(4)}>4</CalcButton>
+                            <CalcButton onClick={() => this.addNumber(5)}>5</CalcButton>
+                            <CalcButton onClick={() => this.addNumber(6)}>6</CalcButton>
+                            <CalcButton onClick={() => this.addNumber(7)}>7</CalcButton>
+                            <CalcButton onClick={() => this.addNumber(8)}>8</CalcButton>
+                            <CalcButton onClick={() => this.addNumber(9)}>9</CalcButton>
+                            <CalcButton onClick={() => this.addNumber(0)}>0</CalcButton>
+                            <CalcButton onClick={() => this.addDecimal()}>.</CalcButton>
                         </div>
                     </div>
                     <div className='functions'>
